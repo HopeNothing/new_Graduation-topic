ce132d580fb98d96dfe0ba60669c872d649e950c
PeterLukGit <peter33633@gmail.com>
2018-01-19 01:41:28 +0800

    20180118

--- new_team_game/Assets/player_conter.cs
+++ new_team_game/Assets/player_conter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DragonBones;
 
 public class player_conter : MonoBehaviour
 {
@@ -12,6 +13,16 @@ public class player_conter : MonoBehaviour
 
     public player player_direction;
 
+    public enum player_animation { none, walk, jump_up, jump_down }
+
+    public player_animation player_dragon_animation;
+
+    public player_animation player_dragon_animation_last;
+
+    public bool animation_is_play;
+
+    public UnityArmatureComponent player_doagon;
+
     public float jump_speed;
 
     public float speed;
@@ -20,9 +31,17 @@ public class player_conter : MonoBehaviour
 
     public bool onground;
 
+
+
+    //public bool jump_bool;
+
+
+
     private void Awake()
     {
         player_rigidbody = this.GetComponent<Rigidbody2D>();
+
+        
 
         Physics2D.IgnoreCollision(this.gameObject.GetComponent<CircleCollider2D>(), GameObject.FindGameObjectWithTag("player").GetComponent<CircleCollider2D>());
     }
@@ -30,7 +49,7 @@ public class player_conter : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        //player_dragon_animation = player_animation.walk;
     }
 
     // Update is called once per frame
@@ -52,6 +71,13 @@ public class player_conter : MonoBehaviour
 
         //this.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, this.GetComponent<Rigidbody2D>().velocity.y);
         move();
+        //dragon_animation();
+
+        //if(jump_bool ==true)
+        //{
+        //    jump_animaion();
+        //}
+        player_static();
     }
 
     private void move()
@@ -59,10 +85,12 @@ public class player_conter : MonoBehaviour
         if (player_direction == player.go_left)
         {
             player_rigidbody.velocity = new Vector2(-speed * Time.deltaTime , player_rigidbody.velocity.y);
+            this.transform.localScale = new Vector3(1, 1, 1);
         }
         else if (player_direction == player.go_right)
         {
             player_rigidbody.velocity = new Vector2(speed * Time.deltaTime, player_rigidbody.velocity.y);
+            this.transform.localScale = new Vector3(-1, 1, 1);
         }
 
 
@@ -75,6 +103,8 @@ public class player_conter : MonoBehaviour
                 {
                     player_rigidbody.velocity += new Vector2(0, jump_speed);
                     onground = false;
+                    //jump_bool = true;
+                    //player_dragon_animation = player_animation.jump_up;
                 }
             }
             else
@@ -85,10 +115,61 @@ public class player_conter : MonoBehaviour
                 {
                     player_rigidbody.velocity += new Vector2(0, jump_speed);
                     onground = false;
+                    //jump_bool = true;
+                    //player_dragon_animation = player_animation.jump_up;
                 }
             }
         }
 
+
+    }
+
+    void player_static()
+    {
+        if(onground == true)
+        {
+            player_dragon_animation = player_animation.walk;
+            if(player_dragon_animation != player_dragon_animation_last)
+            {
+                player_dragon_animation_last = player_dragon_animation;
+                //animation_is_play = true;
+                dragon_animation(player_dragon_animation.ToString());
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if ((onground == false) &&(player_rigidbody.velocity.y > 0))
+        {
+            player_dragon_animation = player_animation.jump_up;
+            if (player_dragon_animation != player_dragon_animation_last)
+            {
+                player_dragon_animation_last = player_dragon_animation;
+                //animation_is_play = true;
+                dragon_animation(player_dragon_animation.ToString());
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if ((onground == false) && (player_rigidbody.velocity.y < 0))
+        {
+            player_dragon_animation = player_animation.jump_down;
+            if (player_dragon_animation != player_dragon_animation_last)
+            {
+                player_dragon_animation_last = player_dragon_animation;
+                //animation_is_play = true;
+                dragon_animation(player_dragon_animation.ToString());
+            }
+            else
+            {
+                return;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -100,9 +181,84 @@ public class player_conter : MonoBehaviour
        
     }
 
+    void dragon_animation(string name)
+    {
+        //print(player_doagon.animationName);
+        //if ((onground == true) && (player_doagon.animationName != "walk"))
+        //{
+        //    player_doagon.animation.GotoAndPlayByTime("walk", 0, 0);
+        //}
+        //else
+        //{
+        //    if ((player_rigidbody.velocity.y > 0) && (onground == false))
+        //    {
+        //        player_doagon.animation.GotoAndPlayByTime("jump_up", 0, 1);
+        //    }
+        //    else if ((player_rigidbody.velocity.y < 0) && (onground == false))
+        //    {
+        //        player_doagon.animation.GotoAndPlayByTime("jump_down", 0, 1);
+        //    }
+        //}
+
+        //if (player_dragon_animation == player_animation.walk)
+        //{
+        //    player_doagon.animation.GotoAndPlayByTime("walk", 0, 0);
+        //    player_dragon_animation = player_animation.none;
+        //}
+
+        //if (player_dragon_animation == player_animation.jump_up)
+        //{
+        //    player_doagon.animation.GotoAndPlayByTime("jump_up", 0, 1);
+        //    player_dragon_animation = player_animation.none;
+        //}
+
+        //if (player_dragon_animation == player_animation.jump_down)
+        //{
+        //    player_doagon.animation.GotoAndPlayByTime("jump_down", 0, 1);
+        //    player_dragon_animation = player_animation.none;
+        //}
+
+        if(name == "walk")
+        {
+            player_doagon.animation.GotoAndPlayByTime("walk", 0, 0);
+        }
+
+        if (name == "jump_up")
+        {
+            player_doagon.animation.GotoAndPlayByTime("jump_up", 0, 1);
+        }
+
+        if (name == "jump_down")
+        {
+            player_doagon.animation.GotoAndPlayByTime("jump_down", 0, 1);
+        }
+
+        //player_doagon.animationName = aaa;
+        //print(player_doagon.animationName);
+    }
+
+    //void jump_animaion()
+    //{
+
+    //}
+
+    //public string aaa;
+
     //private void OnTriggerEnter2D(Collider2D collision)
     //{
     //    print(collision.gameObject.name + collision.gameObject.transform.parent.name);
     //}
+
+    public void change_direction()
+    {
+        if(player_direction == player.go_left)
+        {
+            player_direction = player.go_right;
+        }
+        else
+        {
+            player_direction = player.go_left;
+        }
+    }
 
 }
---
+++ new_team_game/Assets/tower.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tower : MonoBehaviour
+{
+
+    public enum tower_arrow { left, right }
+
+    public tower_arrow tower_direction;
+
+    public GameObject arrow;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (tower_direction == tower_arrow.left)
+        {
+            arrow.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            arrow.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.gameObject.GetComponent<player_conter>().player_direction == player_conter.player.go_left) && (tower_direction == tower_arrow.right))
+        {
+            collision.gameObject.GetComponent<player_conter>().change_direction();
+            tower_direction = tower_arrow.left;
+            arrow.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if ((collision.gameObject.GetComponent<player_conter>().player_direction == player_conter.player.go_right) && (tower_direction == tower_arrow.left))
+        {
+            collision.gameObject.GetComponent<player_conter>().change_direction();
+            tower_direction = tower_arrow.right;
+            arrow.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
